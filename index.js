ea0340dc3c5890842b8cf2a1561deaa4e405f598
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-04-06 21:51:56 -0400

    Src Codes for multiple frameworks

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var world = new CANNON.World();
 var scene = new THREE.Scene();
-world.gravity.set(0, 0, -9.82);
-var cannonDebugRenderer = new THREE.CannonDebugRenderer( scene, world );
-var timeStep = 1.0 / 60.0; // seconds
-// Create and position a camera
+var world = new CANNON.World();
+//var cannonDebugRenderer = new THREE.CannonDebugRenderer( scene, world );
+world.gravity.set(0, -9.82, 0); // m/s²
+world.broadphase = new CANNON.NaiveBroadphase();
+var timeStep = 1.0 / 60.0;
+var meshes = [];
+var bodies = [];
+var material = new THREE.MeshLambertMaterial( { color: 0x777777 } );
+function onWindowResize() {
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize( window.innerWidth, window.innerHeight );
+          controls.screen.width = window.innerWidth;
+          controls.screen.height = window.innerHeight;
+       }
+
 var camera = new THREE.PerspectiveCamera(
     60,                                   // Field of view
     window.innerWidth/window.innerHeight, // Aspect ratio
@@ -11,22 +22,16 @@ var camera = new THREE.PerspectiveCamera(
     1000                                  // Far clipping pane
 );
 
-// Reposition the camera
 camera.position.set(0,30,50);
 
-// Point the camera at a given coordinate
 camera.lookAt(new THREE.Vector3(0,15,0))
 
-// Create a renderer
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Size should be the same as the window
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-// Set a near white clear color (default is black)
-renderer.setClearColor( 'rgb(35, 206, 129)' );
+renderer.setClearColor( 0xfff6e6 );
 
-// Enable shadow mapping
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
@@ -46,28 +51,39 @@ pointLight.shadow.mapSize.height = 1024;
 scene.add( pointLight );
 
 // A basic material that shows the geometry wireframe.
-/*var shadowMaterial = new THREE.ShadowMaterial( { color: 'rgb(223, 244, 26)' } );
-shadowMaterial.opacity = 0.9;*/
-var groundColor = new THREE.MeshStandardMaterial( { color: 'rgb(29, 44, 247)'})
-var groundMesh = new THREE.Mesh(
-    new THREE.BoxGeometry( 200, 0.1, 200 ),
-          groundColor,
-);
-groundMesh.position.y = 0;
-groundMesh.position.x = 0;
-groundMesh.position.z = 0;
-groundMesh.receiveShadow = true;
-scene.add( groundMesh );
+var shape = new CANNON.Plane();
+         var body = new CANNON.Body({ mass: 0 });
+         body.addShape(shape);
+         body.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+         world.addBody(body);
+         bodies.push(body);
 
-shape = new CANNON.Box(new CANNON.Vec3(1,1,1));
-          mass = 1;
-          body = new CANNON.Body({
-            mass: 1
-          });
+var shadowMaterial = new THREE.ShadowMaterial( { color: 0xeeeeee } );
+shadowMaterial.opacity = 0.5;
+var groundMesh = new THREE.Mesh(
+    new THREE.BoxGeometry( 200, .1, 200 ),
+    new THREE.MeshStandardMaterial( {
+       color: 0xff0051,
+   } )
+);
+groundMeshmesh.quaternion.setFromAxisAngle(new THREE.Vector3(1,0,0), -Math.PI / 2);
+groundMesh.receiveShadow = true;
+ meshes.push(groundMesh);
+scene.add( groundMesh );
+///TEST
+var shape = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
+          var body = new CANNON.Body({ mass: mass });
           body.addShape(shape);
-          body.angularVelocity.set(0,0,0);
-          body.angularDamping = 0.5;
+          body.position.set(1,5,0);
           world.addBody(body);
+          bodies.push(body);
+          // Graphics
+          var cubeGeo = new THREE.BoxGeometry( 5, 5, 5, 50, 10 );
+          cubeMesh = new THREE.Mesh(cubeGeo, material);
+          cubeMesh.position.set(12,10,8);
+          meshes.push(cubeMesh);
+          scene.add(cubeMesh);
+          ////TEST_END
 // A simple geometric shape with a flat material
 var shapeOne = new THREE.Mesh(
     new THREE.OctahedronGeometry(10,1),
@@ -83,6 +99,12 @@ shapeOne.rotateZ(Math.PI/3);
 shapeOne.castShadow = true;
 scene.add(shapeOne);
 
+var mass = 5, radius = 1;
+var sphereShape = new CANNON.Sphere(radius); // Step 1
+var sphereBody = new CANNON.Body({mass: mass, shape: sphereShape}); // Step 2
+sphereBody.position.set(12,6,8);
+world.add(sphereBody); // Step 3
+
 // Add a second shape
 var shapeTwo = new THREE.Mesh(
     new THREE.OctahedronGeometry(5,1),
@@ -96,32 +118,40 @@ var shapeTwo = new THREE.Mesh(
 shapeTwo.position.y += 5;
 shapeTwo.position.x += 15;
 shapeTwo.rotateZ(Math.PI/5);
-shapeTwo.name = 'player';
 shapeTwo.castShadow = true;
 scene.add(shapeTwo);
 
 var cube_one = new THREE.Mesh(
-    new THREE.BoxGeometry(10, 10, 10),
+    new THREE.BoxGeometry(3, 3, 3),
     new THREE.MeshStandardMaterial({
-        color: 'rgb(255, 12, 210)',
+        color: 'rgb(216, 104, 23)',
         flatShading: true,
         metalness: 0,
         roughness: 0.8
     })
 );
-cube_one.position.y += 13;
-cube_one.position.x += 29;
+cube_one.position.y += 5;
+cube_one.position.x += 22;
 cube_one.castShadow = true;
 scene.add(cube_one);
 
-function render() {
-    requestAnimationFrame( render );
-    world.step( timeStep );            // Update physics
-    cannonDebugRenderer.update();      // Update the debug renderer
-    renderer.render( scene, camera );  // Render the scene
-}
-render();
+for(var i=0; i !== meshes.length; i++){
+                meshes[i].position.copy(bodies[i].position);
+                meshes[i].quaternion.copy(bodies[i].quaternion);
+            }
 // Render the scene/camera combnation
+function animate(time) {
+            requestAnimationFrame( animate );
+            if(time && lastTime){
+                var dt = time - lastTime;
+                world.step(fixedTimeStep, dt / 1000, maxSubSteps);
+            }
+            updateMeshPositions();
+            cannonDebugRenderer.update();
+            controls.update();
+            renderer.render(scene, camera);
+            lastTime = time;
+        }
 
 // Add an orbit control which allows us to move around the scene. See the three.js example for more details
 // https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/OrbitControls.
