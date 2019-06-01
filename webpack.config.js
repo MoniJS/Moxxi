059ccf4857b6581153bbc9dab698e6db9ca8fa83
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-06-01 19:04:20 -0400

    Fixing.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const buildPath = './build/';
 
 module.exports = {
-  entry: ['./src/entry.js'],
+  entry: ['./src/index.js'],
   output: {
     path: path.join(__dirname, buildPath),
     filename: '[name].[hash].js'
